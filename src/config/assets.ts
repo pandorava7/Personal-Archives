3afe5d5fb9f146567f046f6a74b19c4c044b0f3d
pando <pandora.va7@gmail.com>
2025-12-25 06:49:11 +0800

    change default title

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,2 +1,2 @@
-export const ASSET_BASE_URL = "https://bucket.pandorava7.com";
-// export const ASSET_BASE_URL = "http://localhost:5173/r2";
+// export const ASSET_BASE_URL = "https://bucket.pandorava7.com";
+export const ASSET_BASE_URL = "http://localhost:5173/r2";
